acde65e939c2ccc179c783d057c0104662cd735e
gcruzrios <gcruzr@gmail.com>
2021-03-08 18:37:19 -0600

    Ejercicio 09 FrontEd general

---
+++ src/reducers/calendarReducer.js
@@ -0,0 +1,49 @@
+import moment from 'moment';
+import { types } from '../types/types';
+
+const initialState = {
+    events:[
+        {
+            title:'Cumpleaños del jefe',
+            start: moment().toDate(),
+            end: moment().add(2,'hours').toDate(),
+            bgcolor: '#fafafa',
+            notes: 'Comprar el pastel',
+            user: {
+                _id: '123',
+                name: 'Greivin'
+            }
+        }
+    ],
+    activeEvent:null
+
+}
+
+export const calendarReducer = (state = initialState, action ) => {
+
+    switch (action.type) {
+        
+        case types.eventSetActive:
+            return{
+                ...state,
+                activeEvent: action.payload
+            }
+    
+        case types.eventAddNew:
+            return{
+                ...state,
+                events: [
+                    ...state.events,
+                    action.payload
+                ]
+            }
+
+        case types.eventClearActiveEvent:
+            return{
+                ...state,
+                activeEvent: null
+            } 
+        default:
+            return state;
+    }
+}
---
+++ src/router/AppRouter.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect,
+   
+  } from "react-router-dom";
+import { LoginScreen } from '../components/auth/LoginScreen';
+import { CalendarScreen } from '../components/calendar/CalendarScreen';
+
+  
+export const AppRouter = () => {
+    return (
+        <Router>
+        <div>
+ 
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+            <Route exact path="/login" component={ LoginScreen}/>
+
+              
+            <Route exact path="/" component={CalendarScreen}/>
+
+            <Redirect to="/"/>
+        </Switch>
+      </div>
+    </Router>
+    )
+}
